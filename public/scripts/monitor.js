b1f59b8a7c7adb817cc57ac51463b4f39d672696
Marc Bernabeu <marc.bernabeu@devopensource.com>
2016-02-21 17:54:52 +0100

    monitor logic

--- public/scripts/monitor.js
+++ public/scripts/monitor.js
@@ -2,11 +2,14 @@ var socket = io.connect();
 var startTime;
 var answers;
 var currentQuestion;
+var timeQuestion = 15000;
+var timeResult  = 5000;
+var totalTime = timeQuestion+timeResult;
+var remainingTime;
 
 
 $( document ).ready(function() {
     socket.emit('addmonitor', channel);
-
     startGame();
 });
 
@@ -15,12 +18,8 @@ $( document ).ready(function() {
 function startGame(){
     var endGame = false;
     var questionCount = 1;
-    var timeQuestion = 15000;
-    var timeResult  = 5000;
-    var totalTime = timeQuestion+timeResult;
 
     nextQuestion(0);
-
     var interval = setInterval(function(){
          if(questionCount>questions.length-2){
              endGame=true;
@@ -33,15 +32,12 @@ function startGame(){
     socket.on('updateoption', function (username,data) {
         var answereTime = (new Date).getTime() / 1000;
         var time = parseFloat(answereTime) - parseFloat(startTime);
-        console.log(time);
-
 
         for (i = 0; i < answers.length; i++) {
             if(answers[i].username == username){
                 return false;
             }
         }
-
 
         if(currentQuestion.correct==data){
             //calculate points
@@ -55,10 +51,27 @@ function startGame(){
 }
 
 function nextQuestion(questionCount){
+    $('.answer').removeClass('correct');
+
+    remainingTime=timeQuestion;
+
+    $('#timer span').text(remainingTime/1000);
+
+    console.log(timeQuestion);
+
+    var intervalQuestion = setInterval(function(){
+        remainingTime = remainingTime-1000;
+        if(remainingTime<0){
+            clearInterval(intervalQuestion);
+            showCorrectAnswere();
+        }else{
+            $('#timer span').text(remainingTime/1000);
+        }
+    }, 1000);
+
     socket.emit('unlockallcontroller', 1);
     answers = [];
     startTime = (new Date).getTime() / 1000;
-    console.log(startTime);
     currentQuestion = questions[questionCount];
     $('#question span').text(questions[questionCount].question);
     $('#answer1 span').text(questions[questionCount].answers.option1);
@@ -67,4 +80,9 @@ function nextQuestion(questionCount){
     $('#answer4 span').text(questions[questionCount].answers.option4);
 
     socket.emit('sendanswers', questions[questionCount].answers);
+}
+
+function showCorrectAnswere(){
+    $('#answer'+currentQuestion.correct).addClass('correct');
+    console.log("show correct answere");
 }
